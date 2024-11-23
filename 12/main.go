67fe01854e833fb6e6f19b5733d794e5c773cfd1
jay-1799 <patelj1799@gmail.com>
2024-11-22 19:56:04 -0500

    added ex 12

---
+++ 12/main.go
@@ -0,0 +1,69 @@
+package main
+
+import (
+	"fmt"
+	"os"
+	"path/filepath"
+	"strconv"
+	"strings"
+)
+
+type file struct {
+	name string
+	path string
+}
+
+func main() {
+
+	dir := "sample"
+	var toRename []file
+
+	filepath.Walk(dir, func(path string, info os.FileInfo, err error) error {
+		if info.IsDir() {
+			return nil
+		}
+		if _, err := match(info.Name()); err == nil {
+			toRename = append(toRename, file{
+				name: info.Name(),
+				path: path,
+			})
+		}
+		return nil
+	})
+	for _, f := range toRename {
+		fmt.Printf("%q\n", f)
+	}
+
+	for _, orig := range toRename {
+		var n file
+		var err error
+		n.name, err = match(orig.name)
+		if err != nil {
+			fmt.Println("Error matching:", orig.path, err.Error())
+		}
+		n.path = filepath.Join(dir, n.name)
+		fmt.Printf("mc %s => %s\n", orig.path, n.path)
+
+		err = os.Rename(orig.path, n.path)
+		if err != nil {
+			fmt.Println("Error renaming:", orig.path, err.Error())
+		}
+	}
+}
+
+// returns the new file name or err
+func match(fileName string) (string, error) {
+	// "birthday", "001" ".txt"
+	pieces := strings.Split(fileName, ".")
+	ext := pieces[len(pieces)-1]
+	tmp := strings.Join(pieces[0:len(pieces)-1], ".")
+	pieces = strings.Split(tmp, "_")
+	name := strings.Join(pieces[0:len(pieces)-1], "_")
+	number, err := strconv.Atoi(pieces[len(pieces)-1])
+	if err != nil {
+		return "", fmt.Errorf("%s didn't match our pattern", fileName)
+	}
+
+	// Birthday - 1.txt
+	return fmt.Sprintf("%s - %d.%s", strings.Title(name), number, ext), nil
+}
